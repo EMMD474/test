f026181936892edffc5d4af8d52c703a3bdef03a
EMMNAUEL DALISO BANDA <emmanueldaliso3@gmail.com>
2023-10-03 11:13:27 +0200

    added a function in the test.py file

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 import customtkinter as c
+
+def biggest_number():
+    list = [10, 2, 30, 4, 5, 6, 70, 8 , 9, 10]
+    bn = list[0]
+    for number in list:
+        if number > bn:
+            bn = number
+        else:
+           pass
+    print(bn)
+
+biggest_number()
